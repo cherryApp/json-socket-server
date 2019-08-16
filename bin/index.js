6ded54d1434a0c2e2812957e7ae6b6445e831f8d
Cserkó József <j.cserko@gmail.com>
2019-08-17 01:03:49 +0200

    Feat: message log has been added.

--- bin/index.js
+++ bin/index.js
@@ -5,11 +5,3 @@ const WsServer = require('../lib/ws-server');
 
 // Create socket server.
 const wsServer = new WsServer(cli.input[0], cli.flags.port, cli.flags.watchFile);
-
-/* const test = async () => {
-    let id = await server.save({name: 'Jack', email: 'jack@gmail.com'});
-    console.log(id);
-};
-
-test();
- */
