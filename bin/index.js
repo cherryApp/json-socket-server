953e9726677112bd552d7be0dfe723ba28d76656
cherryApp <j.cserko@gmail.com>
2019-08-16 18:46:23 +0200

    Feat: crud has been created.

--- bin/index.js
+++ bin/index.js
@@ -4,7 +4,7 @@ const cli = require('../lib/cli');
 const WsServer = require('../lib/ws-server');
 
 // Create socket server.
-const wsServer = new WsServer(cli.input[0], cli.flags.port, cli.flags.watch);
+const wsServer = new WsServer(cli.input[0], cli.flags.port, cli.flags.watchFile);
 
 /* const test = async () => {
     let id = await server.save({name: 'Jack', email: 'jack@gmail.com'});
