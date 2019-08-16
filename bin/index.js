d8e38c4453901944ee3a06cd93d8d8377823548f
cherryApp <j.cserko@gmail.com>
2019-08-16 17:34:41 +0200

    Feat: cli parser has been added.

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 
-const server = require('../lib/server');
+const cli = require('../lib/cli');
+const WsServer = require('../lib/ws-server');
 
-const test = async () => {
+// Create socket server.
+const wsServer = new WsServer(cli.input[0], cli.flags.port, cli.flags.watch);
+
+/* const test = async () => {
     let id = await server.save({name: 'Jack', email: 'jack@gmail.com'});
     console.log(id);
 };
 
 test();
+ */
